8ca47af0caafafdac236b2ad7f265b461b93d3f5
hefuren <1740610992@qq.com>
2021-11-17 12:51:39 +0800

    新增返回的实体类

---
+++ src/main/java/com/volunteer/entity/common/Result.java
@@ -0,0 +1,36 @@
+package com.volunteer.entity.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 统一API响应结果封装
+ * @author 何福任
+ */
+
+@Data
+public class Result implements Serializable {
+
+    private int code;
+
+    private String message = "success";
+
+    private Object data;
+
+    public Result setCode(int resultCode){
+        this.code = resultCode;
+        return this;
+    }
+
+    public Result setMessage(String message){
+        this.message = message;
+        return this;
+    }
+
+    public Result setData(Object data){
+        this.data = data;
+        return this;
+    }
+
+}
