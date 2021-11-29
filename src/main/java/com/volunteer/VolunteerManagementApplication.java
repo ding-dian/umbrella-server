1a68bd2e2665bb56181c9b61c6dcde7092244fcd
何福任 <1740610992@qq.com>
2021-11-29 17:18:28 +0800

    引入FastDFS

--- src/main/java/com/volunteer/VolunteerManagementApplication.java
+++ src/main/java/com/volunteer/VolunteerManagementApplication.java
@@ -1,14 +1,21 @@
 package com.volunteer;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * @author HeFuRen
  */
 @SpringBootApplication
 @MapperScan("com.volunteer.mapper")
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class VolunteerManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(VolunteerManagementApplication.class, args);
