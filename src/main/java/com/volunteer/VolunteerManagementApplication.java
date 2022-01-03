c29cf68f1c2485d339e579db29c100d29594730c
梁峰源 <2062728920@qq.com>
2022-01-03 16:27:23 +0800

    添加了http访问重定向方法，添加了数字签名，上线时使用

--- src/main/java/com/volunteer/VolunteerManagementApplication.java
+++ src/main/java/com/volunteer/VolunteerManagementApplication.java
@@ -1,9 +1,15 @@
 package com.volunteer;
 
 import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
@@ -19,4 +25,34 @@ public class VolunteerManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(VolunteerManagementApplication.class, args);
     }
+    //https访问，项目上线时启用
+    //添加转向类，用户将客户访问的非https协议转向到https协议
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        //Connector监听的http的端口号
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        //监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
 }
